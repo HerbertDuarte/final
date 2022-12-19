b96c25a228ffc79fdc4ad5bc9fb3779bd8b1303c
HerbertDuarte <herbertduarte.santos@gmail.com>
2022-12-19 13:57:20 -0300

    correção de media invalida maior do que 10

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function calcular1(){
   var strmedia = document.getElementById('media')
   var media = Number(strmedia.value)
 
-  if( media >= 7){
-    alert('Parabéns! Você foi APROVADO e não precisará de provas finais.')
-  }
-  else if( media < 0 || media > 10){
+  if( media < 0 || media > 10){
     alert('ERRO (Média inválida)')
+  }
+  else if( media >= 7){
+    alert('Parabéns! Você foi APROVADO e não precisará de provas finais.')
   }
   else if(media < 2.5){
     alert('Infelizmente sua média não foi suficiente para ir para as proas finais! :( ')
