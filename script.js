8c03545f3e0cc77cd2ecb92447304c44a16bafcc
Herbert Duarte <115582890+HerbertDuarte@users.noreply.github.com>
2022-12-22 19:39:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 
 function calcular1(){
-  var strmedia = document.getElementById('media')
-  var media = Number(strmedia.value)
-
-  if( media <= 0 || media > 10){
+  var strmedia = document.getElementById('media').value
+  
+  if( strmedia.length == 0){
+    alert('ERRO (Digite um valor válido para a média).')
+  }else{
+  var media = Number(strmedia)
+  if( media > 10){
     alert('ERRO (Média inválida).')
   }
+    
   else if( media >= 7){
     alert('Parabéns! Você foi APROVADO e não precisará de provas finais.')
   }
@@ -16,6 +20,7 @@ function calcular1(){
     var resultado = 15 - (media*2)
     alert(`Você precisará de ${resultado.toFixed(1)} pontos na prova final.`)
   }
+}
 }
 
 function calcular2(){
@@ -29,8 +34,8 @@ function calcular2(){
   var media = (media1 + media2 + media3)/3
 
 
-  if( media <= 0 || media > 10 || media1 <= 0 || media1 > 10 || media2 <= 0 || media2 > 10 || media3 <= 0 || media3 > 10){
-    alert('ERRO (Verifique os valores preenchidos).')
+  if(strmedia1.value.length == 0 || media1 > 10 || strmedia2.value.length == 0 || media2 > 10 || strmedia3.value.length == 0 || media3 > 10){
+    alert('ERRO (Verifique os valores preenchidos). OS 3 campos de notas devem ser preenchidos. As notas devem ter valores de 0 até 10.')
   }
   else if( media >= 7){
     alert(`Media = ${media}.Parabéns! Você foi APROVADO e não precisará de provas finais.`)
